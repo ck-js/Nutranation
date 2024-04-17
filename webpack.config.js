a2f2a2d890efd3343bf78c627624dd183c9118f9
ck-js <ckentertainmentinfo@gmail.com>
2024-04-17 18:37:36 +0700

    remove default padding, margin and box sizing of browsers and add global variables for spacing between elements and sections.

--- webpack.config.js
+++ webpack.config.js
@@ -31,18 +31,18 @@ module.exports = {
       test: /\.(png|svg|jpg|jpeg|gif)$/i,
       type: 'asset/resource',
     },
-    {
-      test: /\.mp4$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]'
-          }
-        },
+    // {
+    //   test: /\.mp4$/,
+    //   use: [
+    //     {
+    //       loader: 'file-loader',
+    //       options: {
+    //         name: '[name].[ext]'
+    //       }
+    //     },
         
-      ]
-    },
+    //   ]
+    // },
 
 
  
